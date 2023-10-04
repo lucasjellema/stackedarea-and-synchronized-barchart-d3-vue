eda6c95ed35460a0c64b539fde6ea82254075ce4
Lucas Jellema <lucasjellema@gmail.com>
2023-10-04 05:10:10 +0000

    Add buttons to switch country collaboration

--- stacked-area-and-synched-bar-chart/src/App.vue
+++ stacked-area-and-synched-bar-chart/src/App.vue
@@ -5,12 +5,21 @@ import { ref, computed, watch } from 'vue';
 const countries = ref([])
 countries.value.push("Belgium")
 
+function handleButtonClick(collboaratingCountries) {
+  countries.value = collboaratingCountries
+}
+
 </script>
 
 <template>
   <div>
     <h1>Collaboration of Countries {{ countries }}</h1>
+    <button @click="handleButtonClick(['Indonesia','Singapore'])">Collaborate Indonesia and Singapore</button>
+    <button @click="handleButtonClick(['Germany','The Netherlands'])">Collaborate Germany and Netherlands</button>
+    <button @click="handleButtonClick(['USA','Canada','Mexico'])">Collaborate USA, Canada, Mexico</button>
+    <div>
     <StackedAreaPlusBar :countries=countries />
+    </div>
   </div>
 </template>
 
