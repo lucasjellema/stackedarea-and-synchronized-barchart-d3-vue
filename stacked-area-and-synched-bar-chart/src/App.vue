9ae1ab5635ce97f4ba1d79f2cbcc07ccd7ce7df5
Lucas Jellema <lucasjellema@gmail.com>
2023-10-26 10:28:40 +0200

    empty selected countries array before changing selection

--- stacked-area-and-synched-bar-chart/src/App.vue
+++ stacked-area-and-synched-bar-chart/src/App.vue
@@ -8,11 +8,12 @@ const countries = ref([])
 const selectedTechnology = ref("")
 const selectedCountry = ref("")
 const isChecked = ref(true);
-const preSelectedCountries = ref(["ID","IN"])
+const preSelectedCountries = ref(["SG"])
 countries.value.push("SG")
 
 function handleButtonClick(collaboratingCountries) {
   countries.value = collaboratingCountries
+  preSelectedCountries.length=0
   preSelectedCountries.value = collaboratingCountries
 }
 
@@ -34,16 +35,18 @@ const collaborationStore = useCollaborationStore();
     <!-- Checkbox to toggle between components -->
     <input type="checkbox" v-model="isChecked"/>Show Area Chart or World Map
     <h1>Collaboration of Countries {{ countries }}</h1>
-    <p>    <button @click="handleButtonClick(['ID', 'SG'])">Collaborate Indonesia and Singapore</button>
+    <p>    
+    <button @click="handleButtonClick(['ID', 'SG'])">Collaborate Indonesia and Singapore</button>
     <button @click="handleButtonClick(['ID'])">Just Indonesia</button>
     <button @click="handleButtonClick(['SG'])">Just Singapore</button>
+    <button @click="handleButtonClick(['PH'])">Just Philippines</button>
   </p>
 
   <div v-if="isChecked">
 
     <p>
-    <button @click="handleButtonClick(['MM', 'PH'])">Collaborate Myanmar and Philipines (FAKE)</button>
-    <button @click="handleButtonClick(['MM'])">Just Myanmar (FAKE)</button>
+    <button @click="handleButtonClick(['IN', 'PH'])">Collaborate India and Philipines (FAKE)</button>
+    <button @click="handleButtonClick(['IN'])">Just India (FAKE)</button>
     <button @click="handleButtonClick(['FAKE'])">Show Fake Data</button>
   </p>
     <div>
